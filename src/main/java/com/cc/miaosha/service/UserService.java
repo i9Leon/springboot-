3847aa8b61af6158dc8aac11bca830a5e8298cee
i9Leon <631284835@qq.com>
2020-05-14 11:09:40 +0800

    sales

---
+++ src/main/java/com/cc/miaosha/service/UserService.java
@@ -0,0 +1,17 @@
+package com.cc.miaosha.service;
+
+import com.cc.miaosha.dao.UserDao;
+import com.cc.miaosha.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    public User getById(int id){
+        return userDao.getById(id);
+    }
+}
